e3268c669abc85e90b97b8be3d887a5537196fd2
yuka-52 <azumome.52@gmail.com>
2022-06-17 00:45:26 +0900

    remove:不要ファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_114808) do
+ActiveRecord::Schema.define(version: 2022_06_15_044013) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
--- spec/factories/comments.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    comment_content { "MyString" }
-    user { nil }
-    search { nil }
-  end
-end
