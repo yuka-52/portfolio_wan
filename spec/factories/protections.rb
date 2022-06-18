bedea172cd5379a528f5cee1fae17062628050e7
yuka-52 <azumome.52@gmail.com>
2022-06-19 00:11:37 +0900

    update: ProtectionModel単体テスト追加

--- spec/factories/protections.rb
+++ spec/factories/protections.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :protection do
     user
     protection_images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'spec/fixtures/test.jpg') ] }
-    protection_title { "MyString" }
-    protection_breed { "MyString" }
-    protection_profile { "MyText" }
-    protection_place { "MyText" }
+    protection_title { "protection_dog" }
+    protection_breed { "siba" }
+    protection_profile { "Text" }
+    protection_place { "Tokyo" }
     protection_date { "2022-06-14" }
-    protection_contact { "MyText" }
-    protection_choker { "MyString" }
-    protection_dogtag { "MyString" }
-    contacted_institution { "MyText" }
+    protection_contact { "Contact" }
+    protection_choker { "No" }
+    protection_dogtag { "Yes" }
+    contacted_institution { "Police" }
   end
 end
