4e26226aff4e21a769143da41439b82bf30ef61b
yuka-52 <azumome.52@gmail.com>
2022-06-19 05:24:54 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -12,44 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_06_15_044013) do
 
-  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "comments", charset: "utf8mb3", force: :cascade do |t|
-    t.string "comment_content"
-    t.bigint "user_id", null: false
-    t.bigint "search_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["search_id"], name: "index_comments_on_search_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "protections", charset: "utf8mb3", force: :cascade do |t|
     t.json "protection_images"
     t.string "protection_title"
@@ -106,8 +68,4 @@ ActiveRecord::Schema.define(version: 2022_06_15_044013) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "searches"
-  add_foreign_key "comments", "users"
 end
