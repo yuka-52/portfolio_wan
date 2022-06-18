fa9a13cccf91fd8a888f2c0e57fe3fdc32745554
yuka-52 <azumome.52@gmail.com>
2022-06-19 02:20:07 +0900

    update: searchRequestsテスト追加

--- spec/factories/searches.rb
+++ spec/factories/searches.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
   factory :search do
+    search_title { "search_pochi" }
     dog_name { "Pochi" }
     dog_breed { "siba" }
     dog_age { 1 }
     dog_profile { "Text" }
     disappeared_place { "Tokyo" }
+    disappeared_date { "2022-06-15" }
+    contact { "Police" }
     dog_images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'spec/fixtures/test.jpg') ] }
   end
 end
