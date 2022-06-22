1803d2a3624cd8790677a781f92f1232a62d1a51
yuka-52 <azumome.52@gmail.com>
2022-06-22 16:15:55 +0900

    CarrierWaveでデフォルト画像の設定

--- app/uploaders/protection_images_uploader.rb
+++ app/uploaders/protection_images_uploader.rb
@@ -18,7 +18,7 @@ class ProtectionImagesUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "default.png"
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   end
 
   def extension_whitelist
