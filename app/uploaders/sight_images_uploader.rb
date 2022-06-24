21f48884c50d8c174d5e14400ca06bbfae549c9b
yuka-52 <azumome.52@gmail.com>
2022-06-24 09:45:20 +0900

    画像表示のための修正

--- app/uploaders/sight_images_uploader.rb
+++ app/uploaders/sight_images_uploader.rb
@@ -18,7 +18,7 @@ class SightImagesUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpeg"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
   end
 
   def extension_whitelist
