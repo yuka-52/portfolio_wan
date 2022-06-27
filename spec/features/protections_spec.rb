193f9bce3a76fda40fa38f79bc2ff2b169b3c781
yuka-52 <azumome.52@gmail.com>
2022-06-27 16:36:51 +0900

    add:default.jpg/rspecテスト修正途中

---
+++ spec/features/protections_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Protections", type: :feature do
+  let!(:protections) { create(:protection, user: user) }
+  let!(:user) { create(:user) }
+
+  scenario "対象のデータが表示されること" do
+      binding.pry
+   expect(page).to have_content protections.protection_title
+   expect(page).to have_content protections.protection_breed
+   expect(page).to have_content protections.protection_choker
+   expect(page).to have_content protections.protection_profile
+   expect(page).to have_content protections.protection_place
+   expect(page).to have_content protections.protection_date
+   expect(page).to have_content protections.protection_contact
+   expect(page).to have_content protections.protection_dogtag
+ end
+
+ scenario "保護犬詳細ページから保護犬一覧ページへのリンクが機能していること" do
+   click_link '一覧ページへ戻る'
+   expect(current_path).to eq protections_index_path
+ end
+end
