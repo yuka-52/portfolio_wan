47918f5df07b6fd365770f9e666245b8b914fb2c
yuka-52 <azumome.52@gmail.com>
2022-07-09 01:39:33 +0900

    update:通知機能修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,20 @@
 class BoardsController < ApplicationController
   def create
-    @board = current_user.boards.build(board_params)
-    @board.sight_id = params[:sight_id]
+    @sight = Sight.find(params[:sight_id])
+    @board = @sight.boards.build(board_params)
+    @board.user_id = current_user.id
+    @board_sight = @board.sight
     if @board.save
-      @sight = @board.sight
-      @sight.create_notification_board!(current_user, @board.id)
+      @board_sight.create_notification_board!(current_user, @board.id)
       redirect_back(fallback_location: root_path)
     else
       redirect_back(fallback_location: root_path)
     end
   end
-end
+
+  private
+
+    def board_params
+      params.require(:board).permit(:board_content, :sight_id)
+    end
+  end
