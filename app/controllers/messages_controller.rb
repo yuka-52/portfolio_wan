16022c1055de20cdc421a8f2132ec63ad40a6af1
yuka-52 <azumome.52@gmail.com>
2022-07-21 22:29:20 +0900

    fix:herokuに表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,38 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!, :only => [:create]
+    def create
+        if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+          @message = Message.new(message_params)
+          # ここから
+          @room=@message.room
+          # ここまでを追加
+          if @message.save
 
-  def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      @room_message = @message.room
-      @message.user_id = current_user.id
-      @room_message.create_notification_dm!(current_user, @message.id)
-    else
-      flash[:alert] = "メッセージ送信に失敗しました。"
+            # ここから
+            @roommembernotme=Entry.where(room_id: @room.id).where.not(user_id: current_user.id)
+            @theid=@roommembernotme.find_by(room_id: @room.id)
+            notification = current_user.active_notifications.new(
+                room_id: @room.id,
+                message_id: @message.id,
+                visited_id: @theid.user_id,
+                visitor_id: current_user.id,
+                action: 'dm'
+            )
+            # 自分の投稿に対するコメントの場合は、通知済みとする
+            if notification.visitor_id == notification.visited_id
+                notification.checked = true
+            end
+            notification.save if notification.valid?
+            # ここまでを追加
+
+            redirect_to "/rooms/#{@message.room_id}"
+          end
+        else
+          redirect_back(fallback_location: root_path)
+        end
     end
-    redirect_to "/rooms/#{@message.room_id}"
-  end
+
+    private
+      def message_params
+          params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id)
+      end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -4,24 +4,5 @@ class Room < ApplicationRecord
   has_many :notifications, dependent: :destroy
   belongs_to :user
 
-  def create_notification_dm!(current_user, message_id)
-    temp_ids = Message.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-      save_notification_dm!(current_user, message_id, temp_id['user_id'])
-    end
-    save_notification_dm!(current_user, message_id) if temp_ids.blank?
-  end
-
-  def save_notification_dm!(current_user, message_id, visited_id)
-    notification = current_user.active_notifications.new(
-      room_id: id,
-      message_id: message_id,
-      visited_id: visited_id,
-      action: 'dm'
-    )
-    if notification.visitor_id == notification.visited_id
-      notification.checked = true
-    end
-    notification.save if notification.valid?
-  end
+ 
 end
