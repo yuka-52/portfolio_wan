b905ce85746ff1430d080a37a1ba3acda25a294f
yuka-52 <azumome.52@gmail.com>
2022-07-21 17:34:17 +0900

    fix:heroku表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,20 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    @message_room = @message.room
+    if @message.save
+      @message_room.create_notification_dm!(current_user, @message.id)
+      redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = "メッセージ送信に失敗しました。"
+      redirect_back(fallback_location: root_path)
     end
-    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:room_id, :content)
   end
 end
