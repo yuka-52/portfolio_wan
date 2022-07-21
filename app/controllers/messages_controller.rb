8200258dff4e9547ca78fb327754e1db119b200a
yuka-52 <azumome.52@gmail.com>
2022-07-21 18:04:30 +0900

    fix:heroku修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,13 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      @message.user_id = current_user.id
       @room_message = @message.room
+      @message.user_id = current_user.id
       if @message.save
         @room_message.create_notification_dm!(current_user, @message.id)
-        redirect_back(fallback_location: root_path)
+          redirect_to "/rooms/#{@message.room_id}"
+      else
+          redirect_back(fallback_location: root_path)
       end
     else
       redirect_back(fallback_location: root_path)
